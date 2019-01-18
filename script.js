e40d28076a65fd3a303aa9cefca4f9b7dccea39a
VitalGol <vital_gol@ukr.net>
2019-01-18 14:05:35 +0200

    v-model.lazy

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		x: 0,
-		y: 0
+		inputValue: 'hi'
 	},
 	watch: {},
 	methods: {
-		handleMouseMove: function (event) {
-			this.x = event.clientX;
-			this.y = event.clientY;
-		},
-		alertValue: function (event) {
-			alert(event.target.value);
-			// if (event.keyCode === 13) {
-      //  alert(event.target.value);
-			// }
-		}
+
 	}
 });
