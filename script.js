3e40762fbbfa00814d4b1ba63c197c34e2868d0a
VitalGol <vital_gol@ukr.net>
2019-01-18 12:40:12 +0200

    v-on

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		link: '<a href="http://google.com">Google</a>'
+		counter: 0,
+		counter1: 10,
+		green: '#0f0',
+		blue: '#00f'
 	},
 	watch: {
 
 	},
 	methods: {
-
+		addCounter: function () {
+			this.counter++;
+		},
+		onHover: function (event) {
+			event.target.style.color = this.green;
+		},
+		onHover1: function (event) {
+			event.target.style.color = this.blue;
+		}
 	}
 });
