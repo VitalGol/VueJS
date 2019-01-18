e9ed46059f6e8f3092118262856908f3abc1cbc6
VitalGol <vital_gol@ukr.net>
2019-01-18 12:12:58 +0200

    v-html

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		message: 'Привет, Vue!',
-		name: 'Vitaly',
-		isOK: true,
-		string: 'ylatiV si eman yM',
-		url: 'http://google.com'
+		link: '<a href="http://google.com">Google</a>'
 	},
 	watch: {
 
 	},
 	methods: {
-		sayHi: function () {
-			return 'I have a good day';
-		}
+
 	}
 });
