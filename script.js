887f45c301a9ca26a03cabc951fd431d626b3370
VitalGol <vital_gol@ukr.net>
2019-01-18 17:18:40 +0200

    v-for for Object list

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		people: [
-			'Helen',
-			'Bob',
-			'Max',
-			'Glasha'
-		]
-
-	},
-	watch: {},
-	computed: {
-
-	},
-	methods: {
+		people: [{
+				name: 'Bob',
+				age: 22,
+				color: 'brown'
+			},
+			{
+				name: 'Helen',
+				age: 62,
+				color: 'brown'
+			},
+			{
+				name: 'Max',
+				age: 52,
+				color: 'brown'
+			},
+			{
+				name: 'Glasha',
+				age: 122,
+				color: 'brown'
+			}
+		],
+		person: {
+			name: 'John',
+			age: 55,
+			job: 'FE dev'
+		}
 
 	}
 });
