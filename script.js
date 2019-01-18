b68090b559e6590aac36ec22fe70ddb46a24f040
VitalGol <vital_gol@ukr.net>
2019-01-18 15:07:47 +0200

    v-if

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		isActive: false,
-		height: 100,
-		color: 'blue'
+		isVisible: true
+
 	},
 	watch: {},
 	computed: {
-		circleClasses: function () {
-			return {
-				"background": this.color,
-				"height": this.height + "px"
-			};
-		}
+
 	},
 	methods: {
 
