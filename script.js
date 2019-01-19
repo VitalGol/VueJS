319122a09c1e3a57cf80393f4e6602dc88505c68
VitalGol <vital_gol@ukr.net>
2019-01-19 15:43:03 +0200

    Жизненный цикл Vue

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 var app = new Vue({
+	el: '#app',
 	data: {
 		title: 'Hello!'
 		},
-		template: '<div><h1>{{ title }}</h1><p>someText</p></div>'
+		beforeCreate() {
+			console.log('before Create');
+		},
+		created() {
+			console.log('Create');
+		},
+		beforeMount() {
+				console.log('before Mount');
+		},
+		mounted() {
+			console.log('mounted');
+		},
+		beforeUpdate() {
+			console.log('before Update');
+		},
+		updated() {
+			console.log('Update');
+		},
+		beforeDestroy() {
+			console.log('before Destroy');
+		},
+		destroyed() {
+			console.log('Destroy');
+		},
+		methods: {
+			doDestroy() {
+				this.$destroy();
+			}
+		},
 });
-
-app.$mount('#app');
