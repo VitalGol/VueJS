bf19fb2a36e77363c28a462127b91e1694daf4e2
VitalGol <vital_gol@ukr.net>
2019-01-19 13:37:38 +0200

    computed:

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		people: [{
-				name: 'Bob',
-				age: 22,
-				color: 'brown'
-			},
-			{
-				name: 'Helen',
-				age: 62,
-				color: 'brown'
-			},
-			{
-				name: 'Max',
-				age: 52,
-				color: 'brown'
-			},
-			{
-				name: 'Glasha',
-				age: 122,
-				color: 'brown'
-			}
-		],
-		person: {
-			name: 'John',
-			age: 55,
-			job: 'FE dev'
-		}
+		counter: 0,
+		counter1: 0
 
+	},
+	computed: {
+		computedResult: function () {
+			console.log('computedResult');
+			return this.counter > 5 ? 'More than 5' : 'Less than 5';
+		}
+	},
+	methods: {
+		addCounter: function() {
+			this.counter++;
+		},
+		subCounter: function () {
+			this.counter--;
+		},
+		result: function() {
+			console.log('result');
+			return this.counter > 5 ? 'More than 5' : 'Less than 5';
+		}
 	}
 });
