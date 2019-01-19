f09893954d1f17d8d735d9212526fea7a6f58172
VitalGol <vital_gol@ukr.net>
2019-01-19 14:45:36 +0200

    change date in another div

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 var app = new Vue({
 	el: '#app',
 	data: {
-		counter: 0,
-		counter1: 0
-
-	},
-	computed: {
-		computedResult: function () {
-			console.log('computedResult');
-			return this.counter > 5 ? 'More than 5' : 'Less than 5';
-		}
-	},
-	watch: {
-		counter: function(value) {
-			console.log('this.counter = ' + value);
-		}
+		title: 'Hello'
 	},
 	methods: {
-		addCounter: function() {
-			this.counter++;
-		},
-		subCounter: function () {
-			this.counter--;
-		},
-		result: function() {
-			console.log('result');
-			return this.counter > 5 ? 'More than 5' : 'Less than 5';
+		alertFast: () => {
+     alert('I am from APP');
+		}
+	}
+});
+
+app.newField = 'someText';
+// console.log(app);
+
+var app1 = new Vue({
+	el: '#app1',
+	data: {
+		title: 'Hello Vitaly'
+	},
+	methods: {
+		updateTitle: () => {
+      app.title = 'updated from second app';
+			app.newField = 'New field';
+			setTimeout(() => {
+				app.alertFast();
+			}, 3000);
 		}
 	}
 });
